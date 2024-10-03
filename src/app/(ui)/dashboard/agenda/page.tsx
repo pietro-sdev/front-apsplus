8d8853ab5999d19e0acfb0e94e4006508b6ac4e1
Pietro <pietro.dsantos77@gmail.com>
2024-10-03 16:13:54 -0300

    Corrigindo agenda

--- src/app/(ui)/dashboard/agenda/page.tsx
+++ src/app/(ui)/dashboard/agenda/page.tsx
@@ -1,11 +1,6 @@
 // app/dashboard/page.tsx
-import SidebarWrapper from '@/components/ui/sidebarwrapper';
-
 export default function DashboardPage() {
   return (
-    <SidebarWrapper>
       <h1>AGENDA</h1>
-      {/* Conteúdo adicional da página */}
-    </SidebarWrapper>
   );
 }
